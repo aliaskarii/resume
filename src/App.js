9045f6d85ee13cc21843ba1192d36c0ae0df88ce
aliaskari76 <askari1791@hotmail.com>
2023-11-27 15:51:03 -0500

    init resume

--- src/App.js
+++ src/App.js
@@ -1,25 +1,381 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='flex m-0 font-sans antialiased font-normal text-base leading-default min-h-screen'>
+      {/* <nav>
+        <div className="block">
+          <h1 className="mb-0 text-5xl font-bold text-gray-750">
+            ALI ASKARI
+          </h1>
+          <h2 className="m-0 ml-2 text-2xl font-semibold text-gray-700 leading-snugish">
+            Front End Developer
+          </h2>
+          <h3 className="m-0 mt-2 ml-2 mb-2 font-semibold text-md text-gray-550 leading-snugish">
+            Iran
+          </h3>
+        </div>
+        <div className="justify-between px-3 mt-0 mb-5 text-3xl font-bold leading-none text-gray-200 initials-container bg-gray-750 print:bg-black">
+          <div className="text-center initial">R</div>
+          <div className="text-center initial">P</div>
+        </div>
+        <section className="pb-4 mt-4 first:mt-0">
+              <div className="break-inside-avoid">
+                <section className="pb-2 mb-2 border-b-2 break-inside-avoid">
+                  <ul className="list-inside pr-7">
+
+                    <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700">
+                      <a href="mailto:me@xigma.dev" className="group">
+                        <span className="mr-8 text-xl font-semibold text-gray-700 leading-snugish">
+                          Email:
+                        </span>
+                        me@xigma.dev
+                        <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">
+                          ↗
+                        </span>
+                      </a>
+                    </li>
+                    <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700">
+                      <a href="tel:+989153343508">
+                        <span className="mr-5 text-xl font-semibold text-gray-700 leading-snugish">
+                          Phone:
+                        </span>
+                        +98 915 334 3508
+                      </a>
+                    </li>
+                  </ul>
+                </section>
+              </div>
+            </section>
+      </nav> */}
+
+      <aside className='bg-gray-400  h-min-screen w-full max-w-[20rem] p-8  transition-transform duration-300 xl:translate-x-0 border border-blue-gray-100 flex flex-col justify-start items-center'>
+        <div className="break-inside-avoid">
+          <h2 className="mb-2 px-4 pt-4 text-xl font-bold tracking-widest text-black print:font-normal">
+            INFO
+          </h2>
+          <hr></hr>
+          <section className="mb-2 break-inside-avoid">
+            <div className="my-1 last:pb-1">
+              <ul className="flex flex-wrap text-sm2 leading-relaxed -mr-1.6 -mb-1 mt-2.1">
+                <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700 py-3">
+                  <a href="tel:+989153343508">
+                    <span className="mr-5 text-xl font-semibold text-black leading-snugish">
+                      Phone:
+                    </span>
+                    +98 915 334 3508
+                  </a>
+                </li>
+                <li className="mt-1 leading-normal transition duration-100 ease-in text-gray-550 text-md hover:text-gray-700 py-3">
+                  <a href="mailto:me@xigma.dev" className="group">
+                    <span className="mr-8 text-xl font-semibold text-black leading-snugish">
+                      Email:
+                    </span>
+                    me@xigma.dev
+                    <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">
+                      ↗
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div className="break-inside-avoid">
+          <h2 className="mb-2 text-xl font-bold tracking-widest text-black print:font-normal px-4 pt-4">
+            SKILLS
+          </h2>
+          <hr></hr>
+          <section className="mb-2 break-inside-avoid">
+            <div className="my-1 last:pb-1">
+              <ul className="flex flex-wrap text-sm2 leading-relaxed -mr-1.6 -mb-1 mt-2.1">
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  HTML5
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  CSS3
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  React
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  BOOTSTRAP
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  Tailwind.css
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  Javascript
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  Python
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  Git
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  Linux
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  Docker
+                </li>
+                <li className="text-md px-2.5 py-0.5 mr-1.6 mb-1 text-black leading-relaxed print:bg-white print:border-inset bg-gray-250">
+                  REST API
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </aside >
+      <main className="w-full flex flex-wrap justify-evenly p-6">
+        <div className="max-w-5xl p-3 mx-auto my-auto">
+
+
+          <div className="overflow-scroll col-gap-16 md:col-count-2 print:col-count-2 md:h-letter-col-full print:h-letter-col-full col-fill-balance">
+
+            <section className="pb-2 mt-4 border-b-4 first:mt-0">
+              <div className="break-inside-avoid">
+                <h2 className="mb-2 text-xl font-bold tracking-widest text-gray-700 print:font-normal">
+                  SUMMARY
+                </h2>
+
+                <section className="mb-2 break-inside-avoid">
+                  <p className="mt-2 leading-normal text-gray-700 text-md">
+                    Front-End Developer with proven experience at Netizens Technologies in helping companies create and maintain a better code base for reusability. Passionate about learning and development .Eager to tackle more complex problems and continue to find ways to maximize user efficiency.
+                  </p>
+                </section>
+              </div>
+            </section>
+            <section className="pb-2 mt-4 border-b-4 first:mt-0">
+              <div className="break-inside-avoid">
+                <h2 className="mb-2 text-xl font-bold tracking-widest text-gray-700 print:font-normal">
+                  ABILITIES
+                </h2>
+                <section className="mb-2 break-inside-avoid">
+                  <ul className="mt-2.1 text-md text-gray-700 leading-normal">
+                    <li>
+                      <span className="transform -translate-y-px select-none text-gray-550">
+                        ›
+                      </span>
+                      Create Responsive Site With W3C Standards.
+                    </li>
+                    <li>
+                      <span className="transform -translate-y-px select-none text-gray-550">
+                        ›
+                      </span>
+                      Crafted literate, Comprehensive Source Code.
+                    </li>
+                    <li>
+                      <span className="transform -translate-y-px select-none text-gray-550">
+                        ›
+                      </span>
+                      Provided commentary to improve maintainability
+                    </li>
+                    <li>
+                      <span className="transform -translate-y-px select-none text-gray-550">
+                        ›
+                      </span>
+                      Configure Linux servers and web server applications
+                    </li>
+                    <li>
+                      <span className="transform -translate-y-px select-none text-gray-550">
+                        ›
+                      </span>
+                      Experience developing indicator and expert adviser with mql language  
+                    </li>
+                    <li>
+                      <span className="transform -translate-y-px select-none text-gray-550">
+                        ›
+                      </span>
+                      Configure Linux servers and web server applications
+                    </li>
+                  </ul>
+                </section>
+              </div>
+            </section>
+            <section className="pb-2  mt-4 border-b-4 first:mt-0">
+              <div className="break-inside-avoid">
+
+                <h2 className="mb-2 text-xl font-bold tracking-widest text-gray-700 print:font-normal">
+                  PROJECTS
+                </h2>
+
+                <section className="pb-4 mb-4 border-b-2 break-inside-avoid">
+                  <header>
+                    <h3 className="text-xl font-semibold text-gray-700 leading-snugish">
+                      <a href="#!" className="group">
+                        Admin Dashboard 
+                      </a>
+                    </h3>
+                    <p className="leading-normal text-md text-gray-550">
+                      2023 | React, Material Tailwind, HTML, CSS
+                    </p>
+                  </header>
+                  <p className="mt-2.1 text-md text-gray-700 leading-normal">
+                    A Personal Statistics Admin Dashboard Project with React Javascript and Material Tailwind CSS  
+                  </p>
+                </section>
+                <section className="pb-4 mb-4 border-b-2 break-inside-avoid">
+                  <header>
+                    <h3 className="text-xl font-semibold text-gray-700 leading-snugish">
+                      <a href="#!" className="group">
+                        TrexLevel 
+                      </a>
+                      <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">
+                      </span>
+                    </h3>
+                    <p className="leading-normal text-md text-gray-550">
+                      2022 | MQL4 
+                    </p>
+                  </header>
+                  <p className="mt-2.1 text-md text-gray-700 leading-normal">
+                    Metatrader indicator to calculate and draw daily level based on Trex Price Action method using mql4 
+                  </p>
+                </section>
+                <section className="pb-4 mb-4 border-b-2 break-inside-avoid">
+                  <header>
+                    <h3 className="text-xl font-semibold text-gray-700 leading-snugish">
+                      <a href="#!" className="group">
+                        Iran Stock Market Perdiction with LSTM
+                      </a>
+                      <span className="inline-block font-normal transition duration-100 ease-in text-gray-550 print:text-black group-hover:text-gray-700">
+                      </span>
+                    </h3>
+                    <p className="leading-normal text-md text-gray-550">
+                      2020 | Python , JupyterLab
+                    </p>
+                  </header>
+                  <p className="mt-2.1 text-md text-gray-700 leading-normal">
+                    LSTM algorithm for predicting iran stock market  
+                  </p>
+                </section>
+              </div>
+            </section>
+            <section className="pb-2 mt-4 first:mt-0">
+              <div className="break-inside-avoid">
+                <h2 className="mb-2 text-xl font-bold tracking-widest text-gray-700 print:font-normal">
+                  EDUCATION
+                </h2>
+                <section className="pb-4 mt-4 mb-4 border-b-2 break-inside-avoid">
+                  <header>
+                    <h3 className="flex-grow text-xl font-semibold text-gray-700 leading-snugish">
+                      Birjand University Of Technology
+                    </h3>
+                    <p className="leading-normal text-md text-gray-550">
+                      2016 – 2021 | Bachelore of Science
+                    </p>
+                  </header>
+                  <p className="mt-1 leading-normal text-md text-gray-650">
+                    <span className="font-semibold text-gray-700 text-md leading-snugish">
+                      Major:
+                    </span>
+                    Computer Science
+                  </p>
+
+                </section>
+              </div>
+            </section>
+            <section className="pb-2 mt-4 first:mt-0">
+              <div className="break-inside-avoid">
+                <h2 className="mb-2 text-xl font-bold tracking-widest text-gray-700 print:font-normal">
+                  LANGUAGE
+                </h2>
+                <section className="pb-4 mt-4 mb-4 border-b-2 break-inside-avoid flex flex-row justify-start">
+                  <div className=''>
+                    <h3 className="flex-grow text-xl font-semibold text-gray-700 leading-snugish">
+                      Perisan
+                    </h3>
+                    <p className="leading-normal text-md text-gray-550">
+                      Native
+                    </p>
+                  </div>
+                  <div className='ml-20'>
+                    <h3 className="flex-grow text-xl font-semibold text-gray-700 leading-snugish">
+                      English
+                    </h3>
+                    <p className="leading-normal text-md text-gray-550">
+                      Intermediate
+                    </p>
+                  </div>
+                </section>
+              </div>
+            </section>
+          </div>
+
+        </div>
+
+      </main>
+    </div >
+  )
 }
 
-export default App;
+export default App
+{/* <section className="pb-4 mt-4 first:mt-0">
+ */}
+// <div className="container mx-auto m-0 font-sans antialiased font-normal text-base leading-default min-h-screen">
+//   <table className="table-auto">
+//     <tbody>
+//       <tr>
+//         <td>
+//           <h2>INFO</h2>
+//           <hr></hr>
+//           <div className="flex flex-row justify-start">
+//             <h1>Phone : </h1>
+//             <h2>+98 915 334 3508</h2>
+//           </div>
+//           <div className="flex flex-row justify-start">
+//             <h1>Email :</h1>
+//             <a href="email:me@xigma.dev">me@xigma.dev</a>
+//           </div>
+//           <div className="flex flex-row justify-start">
+//             <h1>skype :</h1>
+//             <h2>cloner1376</h2>
+//           </div>
+//         </td>
+//         <td>profile</td>
+//       </tr>
+//       <tr>
+//         <td rowspan="5">
+//           <h2>SKILLS</h2>
+//           <hr></hr>
+//           <p>
+//             HTML ,CSS , React , Javascript , Git ,Linux ,Python ,Docker
+//           </p>
+//         </td>
+//         <td>achivement</td>
+//       </tr>
+//       <tr>
+//         <td>abilities</td>
+//       </tr>
+//       <tr>
+//         <td>
+//           <h2>EDUCATION</h2>
+//           <hr></hr>
+//           <p>
+//             <h2>Software Engineering</h2>
+//             <h1>Bachelore of Science</h1>
+//             <h1>Birjand University of Technology, Iran</h1>
+//           </p>
+//         </td>
+//       </tr>
+//       <tr>
+//         <td>
+//           <h2>LAMGUAGE</h2>
+//           <hr></hr>
+//           <div className="flex flex-row justify-between">
+//             <div className="flex flex-row justify-start">
+//               <h2>Perisan : </h2>
+//               <h1>Native</h1>
+//             </div>
+//             <div className="flex flex-row justify-start">
+//               <h2>English : </h2>
+//               <h1>Intermediate</h1>
+//             </div>
+//           </div>
+//         </td>
+//       </tr>
+//       <tr>
+//         <td></td>
+//       </tr>
+//     </tbody>
+//   </table>
+// </div>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = ({
+  darkMode: 'class',
+  content: ['./src/**/*.{html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito sans', 'sans-serif'],
+      },
+      colors: {
+        xiketic: '#060818',
+      },
+    },
+  },
+
+  plugins: [],
+})
